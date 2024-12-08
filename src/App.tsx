8c70b04e65bc6d2ef687c79e2e99eac8444ede1e
LGiki <qq547221432@gmail.com>
2024-12-08 22:31:53 +0800

    Added a toast message: added to favorite scrollers

--- src/App.tsx
+++ src/App.tsx
@@ -97,6 +97,7 @@ export default function App() {
                   return;
                 }
                 favoriteScollersStore.add(scrollerEditorStore.scrollerConfig);
+                toast.success(t("toast.addedToFavoriteScrollers"))
               }}
             >
               <Star />
